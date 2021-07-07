8cbd9278bdc186fe6c536134e2ecf25b06abf17a
Saurav Adhikari <sauravads123@gmail.com>
2021-07-07 17:04:34 +0545

    testing output url

--- infra/main.tf
+++ infra/main.tf
@@ -3,3 +3,17 @@ resource "google_app_engine_application" "app" {
   project     = "${var.project}"
   location_id = "${var.region}"
 }
+# 
+resource "google_app_engine_standard_app_version" "app_engine_next" {
+  version_id = "v1"
+  service    = "default"
+  runtime    = "nodejs14"
+
+  deployment={
+    zip{
+      source_url = {var.project}
+    }
+  }
+
+  delete_service_on_destroy = true
+}
