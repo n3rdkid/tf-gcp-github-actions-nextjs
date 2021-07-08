52e3fc7ae7645cf9b078f010f3df9b4c016d494e
Saurav Adhikari <sauravads123@gmail.com>
2021-07-08 17:40:51 +0545

    TEST

--- infra/main.tf
+++ infra/main.tf
@@ -2,12 +2,26 @@
 resource "google_app_engine_application" "app" {
   project     = "${var.project}"
   location_id = "${var.region}"
-  serving_status = "USER_DISABLED"
+  serving_status = "SERVING"
 }
 # 
 resource "google_app_engine_standard_app_version" "app_engine_next" {
   version_id = "v1"
   service    = "default"
+  runtime    = "nodejs14"
+
+  deployment{
+    zip{
+      source_url = var.artifacts_url
+    }
+  }
+
+  delete_service_on_destroy = true
+}
+
+resource "google_app_engine_standard_app_version" "app_engine_next1" {
+  version_id = "v1"
+  service    = "my"
   runtime    = "nodejs14"
 
   deployment{
